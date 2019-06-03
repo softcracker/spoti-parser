122c3689bef6b5409fb2d13570c0f185a88cbc97
Marlena Flüh <marlena.flueh@white-paper-media.de>
2019-06-03 16:00:09 +0200

    build

---
+++ build/precache-manifest.c0f7f3e90f9645a8cbd43ca2bcfbddda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24fdbc292fd75ff5ffa0325fddd43d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf1841530f12b76d99f0",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a30e43a10b46f66d351c",
+    "url": "/static/css/main.35cb467a.chunk.css"
+  },
+  {
+    "revision": "bf1841530f12b76d99f0",
+    "url": "/static/js/2.02a0443e.chunk.js"
+  },
+  {
+    "revision": "a30e43a10b46f66d351c",
+    "url": "/static/js/main.1e93cc93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
