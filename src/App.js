5868455794bb71ca9961c45d9c7a055126e035ac
Marlena Flüh <marlena.flueh@white-paper-media.de>
2019-06-05 17:06:16 +0200

    rebase

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ class App extends Component {
           json.map(obj => {
           return axios.post(api, obj, {
             headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
                 "x-access-token": this.state.token
             }
           })
@@ -76,6 +77,7 @@ class App extends Component {
     try {
       const res = await axios.get(api + "number", {
         headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
             "x-access-token": token
         }
       })
