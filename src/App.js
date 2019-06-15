55dce0ed2845a18b3c8c3a0e4864a67ab2773498
MarlenaFlueh <m.fluehh@gmail.com>
2019-06-15 13:45:03 +0200

    add dynamic route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 import axios from 'axios'
-import {Form, FormGroup, Col, Input, Button} from 'reactstrap'
+import { Form, FormGroup, Col, Input, Button } from 'reactstrap'
 import './App.css'
-const api = "https://spoti-api.herokuapp.com/"
 //const api = "http://localhost:8080/"
 
 class App extends Component {
@@ -14,18 +13,20 @@ class App extends Component {
     amountPlaceholder: "Amount of data:",
     email: "",
     password: "",
-    emailPlaceholder: "email",
-    passwordPlaceholder: "password",
     token: null,
     authenticated: false,
-    loginMessage: "Login data:"
+    loginMessage: "Login data:",
+    cluster: "user/"
   }
+
+  api = "https://spoti-api.herokuapp.com/" + this.state.cluster
+
 
   componentDidMount = async () => {
     const localToken = localStorage.getItem("token")
     const res = await this.getAmount(localToken)
     if (res !== null && res.status === 200) {
-      this.setState({token: localToken, authenticated: true})
+      this.setState({ token: localToken, authenticated: true })
     }
   }
 
@@ -34,15 +35,15 @@ class App extends Component {
       let dataList = []
       const textByLine = input.toString().split("\n")
       textByLine.forEach(line => {
-          const splitIndex = line.indexOf('|')
-          const string = line.substring(0, splitIndex)
-          const splitted = string.split(':')
-      
-          const obj = {
-              "username": splitted[0].trim(),
-              "password": splitted[1].trim()
-          }
-          dataList.push(obj)
+        const splitIndex = line.indexOf('|')
+        const string = line.substring(0, splitIndex)
+        const splitted = string.split(':')
+
+        const obj = {
+          "username": splitted[0].trim(),
+          "password": splitted[1].trim()
+        }
+        dataList.push(obj)
       })
       return dataList
     } catch (e) {
@@ -57,27 +58,27 @@ class App extends Component {
 
     if (json !== null) {
       try {
-          json.map(obj => {
-          return axios.post(api, obj, {
+        json.map(obj => {
+          return axios.post(this.api, obj, {
             headers: {
               "authorization": "Bearer " + token
             }
           })
         })
-        this.setState({inputPlaceholder: "Sending was successful.", input: ""})
+        this.setState({ inputPlaceholder: "Sending was successful.", input: "" })
       } catch (e) {
-        this.setState({inputPlaceholder: "Error occured.", input: ""})
+        this.setState({ inputPlaceholder: "Error occured.", input: "" })
       }
     } else {
-      this.setState({inputPlaceholder: "Invalid input.", input: ""})
+      this.setState({ inputPlaceholder: "Invalid input.", input: "" })
     }
   }
 
   getAmount = async (token) => {
     try {
-      const res = await axios.get(api + "number", {
+      const res = await axios.get(this.api + "number", {
         headers: {
-            "authorization": "Bearer " + token
+          "authorization": "Bearer " + token
         }
       })
       if (res.status === 200) {
@@ -86,7 +87,7 @@ class App extends Component {
           amountPlaceholder: "Amount of data:"
         })
       } else {
-        this.setState({amountPlaceholder: "Error occured."})
+        this.setState({ amountPlaceholder: "Error occured." })
       }
       return res
     } catch (e) {
@@ -96,69 +97,69 @@ class App extends Component {
 
   login = async () => {
     try {
-      const res = await axios.post(api + "auth", {
-            "email": this.state.email,
-            "password": this.state.password
+      const res = await axios.post(this.api + "auth", {
+        "email": this.state.email,
+        "password": this.state.password
       })
       console.log(res)
       if (res.status === 200) {
         this.setState({
-          token:  res.data.data.token, 
+          token: res.data.data.token,
           authenticated: true,
           password: "",
           email: ""
         })
         localStorage.setItem('token', res.data.data.token)
       } else {
-        this.setState({password: "", email: "", loginMessage: "Invalid input."})
+        this.setState({ password: "", email: "", loginMessage: "Invalid input." })
         console.log(res)
       }
     } catch (e) {
-      this.setState({password: "", email: "", loginMessage: "Error occured."})
+      this.setState({ password: "", email: "", loginMessage: "Error occured." })
       console.log(e)
     }
   }
 
   deleteAll = async (token) => {
     try {
-      await axios.delete(api, {
+      await axios.delete(this.api, {
         headers: {
-            "authorization": "Bearer " + token
+          "authorization": "Bearer " + token
         }
       })
       await this.getAmount(this.state.token)
     } catch (err) {
-      this.setState({amountPlaceholder: "Error occured."})
+      this.setState({ amountPlaceholder: "Error occured." })
     }
   }
 
   render() {
 
     const loginForm = (
-        <Form  className="App-form">
-          <p>{this.state.loginMessage}</p>
-          <FormGroup row>
-            <Col xl={12} style={{marginBottom: 10}}>
-              <Input
-                style={{height: 40, width: "100%" }}
-                type="text" name="text"
-                value={this.state.email}
-                placeholder={this.state.emailPlaceholder}
-                id="exampleText"
-                onChange={(event) => this.setState({email: event.target.value})}
-              />
-            </Col>
-            <Col xl={12}>
-              <Input style={{height: 40, width: "100%" }}
-                type="password" name="password"
-                value={this.state.password}
-                placeholder={this.state.passwordPlaceholder}
-                id="examplePwd" onChange={(event) => this.setState({password: event.target.value})}
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-          <Col  xl={12}>
+      <Form className="App-form">
+        <p>{this.state.loginMessage}</p>
+        <FormGroup row>
+          <Col xl={12} style={{ marginBottom: 10 }}>
+            <Input
+              style={{ height: 40, width: "100%" }}
+              type="text" name="text"
+              value={this.state.email}
+              placeholder="email"
+              id="exampleText"
+              onChange={(event) => this.setState({ email: event.target.value })}
+            />
+          </Col>
+          <Col xl={12}>
+            <Input style={{ height: 40, width: "100%" }}
+              type="password" name="password"
+              value={this.state.password}
+              placeholder="password"
+              id="examplePwd" onChange={(event) => this.setState({ password: event.target.value })}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Col xl={12}>
             <Button onClick={this.login}>Login</Button>
           </Col>
         </FormGroup>
@@ -167,40 +168,40 @@ class App extends Component {
 
     const inputForm = (
       <Fragment>
-        <Form  className="App-form">
-            <Col xl={6}>
-              <p>{this.state.amountPlaceholder}</p>
-            </Col>
+        <Form className="App-form">
+          <Col xl={6}>
+            <p>{this.state.amountPlaceholder}</p>
+          </Col>
           <FormGroup row>
             <Col xl={6}>
               <p>{this.state.amount}</p>
             </Col>
           </FormGroup>
           <FormGroup row>
-          <Col  xl={6}>
-            <Button style={{marginBottom: "20px"}} onClick={() => this.getAmount(this.state.token)}>Get amount</Button>
-          </Col>
-          <Col  xl={6}>
-            <Button color="danger" onClick={() => this.deleteAll(this.state.token)}>Delete all</Button>
-          </Col>
-        </FormGroup>
+            <Col xl={6}>
+              <Button style={{ marginBottom: "20px" }} onClick={() => this.getAmount(this.state.token)}>Get amount</Button>
+            </Col>
+            <Col xl={6}>
+              <Button color="danger" onClick={() => this.deleteAll(this.state.token)}>Delete all</Button>
+            </Col>
+          </FormGroup>
         </Form>
-        <Form  className="App-form">
+        <Form className="App-form">
           <p>New data:</p>
           <FormGroup row>
             <Col xl={12}>
-              <Input style={{height: 300, width: "100%" }}
+              <Input style={{ height: 300, width: "100%" }}
                 type="textarea" value={this.state.input}
                 name="text" placeholder={this.state.inputPlaceholder}
-                id="exampleText" onChange={(event) => this.setState({input: event.target.value})}
+                id="exampleText" onChange={(event) => this.setState({ input: event.target.value })}
               />
             </Col>
           </FormGroup>
           <FormGroup row>
-          <Col  xl={12}>
-            <Button onClick={() => this.sendData(this.state.token)}>Submit</Button>
-          </Col>
-        </FormGroup>
+            <Col xl={12}>
+              <Button onClick={() => this.sendData(this.state.token)}>Submit</Button>
+            </Col>
+          </FormGroup>
         </Form>
       </Fragment>
     )
@@ -211,7 +212,7 @@ class App extends Component {
           {this.state.authenticated ? inputForm : loginForm}
         </header>
       </div>
-    ) 
+    )
   }
 }
 
